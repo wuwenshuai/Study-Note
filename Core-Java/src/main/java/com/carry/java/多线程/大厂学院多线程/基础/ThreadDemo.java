1383f3529fdd2aea449a6bb8b335bcc9880397a3
Carry.W.Wu <carry.w.wu@newegg.com>
2022-02-08 15:16:23 +0800

    fix2

---
+++ Core-Java/src/main/java/com/carry/java/多线程/大厂学院多线程/基础/ThreadDemo.java
@@ -0,0 +1,30 @@
+package com.carry.java.多线程.大厂学院多线程.基础;
+
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+/**
+ * @author cw3k
+ * @version 1.0
+ * @description: TODO
+ * @date 2022/2/8 14:23
+ */
+public class ThreadDemo {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+
+        FutureTask<Integer> futureTask = new FutureTask<Integer>(() -> {
+            Thread.sleep(4000);
+            System.out.println("thread:"+Thread.currentThread().getName());
+
+            return 1024;
+        });
+        new Thread(futureTask,"a").start();
+
+        System.out.println("thread main:"+Thread.currentThread().getName()+"\t"+Thread.currentThread().isDaemon());
+    }
+}
+
+
