e93730ee90e72c4c50581c9553a1cbc5d5c0e10c
cw3k <Carry.W.Wu@newegg.com>
2022-07-05 18:01:02 +0800

    折半

---
+++ 数据结构与算法/src/main/java/com/carrry/search/BinarySearch.java
@@ -0,0 +1,28 @@
+package com.carrry.search;
+
+/**
+ * @author cw3k
+ * @version 1.0
+ * @description: 二分查找，又叫折半查找，建立在有序的集合基础上，先要找到元素的中间值mid，然后拿它和要找的元素进行比较，如果比这个数大，就再另外一半查找
+ * 时间复杂度：: log2( N )
+ * @date 2022/7/5 17:42
+ */
+public class BinarySearch {
+    public static void main(String[] args) {
+
+
+
+
+    }
+    int search(int nums[], int target){
+        int n = nums.length;
+        // 定义左右指针
+        int left = 0;
+        int right = n-1;
+
+        while (left <=right) {
+
+        }
+    }
+
+}
