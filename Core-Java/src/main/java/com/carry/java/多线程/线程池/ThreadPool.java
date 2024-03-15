ba020473e31db9c6e52cc955b0d12567da9a15c0
wws <1096080443@qq.com>
2024-03-15 22:41:36 +0800

    线程池源码学习

--- Core-Java/src/main/java/com/carry/java/多线程/线程池/ThreadPool.java
+++ Core-Java/src/main/java/com/carry/java/多线程/线程池/ThreadPool.java
@@ -1,7 +1,6 @@
 package com.carry.java.多线程.线程池;
 
 
-
 import java.util.concurrent.*;
 
 /**
@@ -14,11 +13,52 @@ public class ThreadPool {
 
     private ExecutorService queryPool = Executors.newFixedThreadPool(2000);
 
-    public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(2000);
-        executorService.submit(() -> {
-            System.out.println("0000000000000000");
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+
+        System.out.println(Runtime.getRuntime().availableProcessors());
+
+
+
+/**
+ * int corePoolSize,
+ *                               int maximumPoolSize,
+ *                               long keepAliveTime,
+ *                               TimeUnit unit,
+ *                               BlockingQueue<Runnable> workQueue
+ */
+
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(5, 10, 2000L, TimeUnit.MICROSECONDS, new ArrayBlockingQueue<>(10)
+                , r -> {
+                    Thread thread = new Thread(r);
+                    thread.setName("test-thread");
+                    return thread;
+                }, new MyRejectExec());
+        // 让线程池处理没有返回结果的
+        threadPoolExecutor.execute(() -> {
+            System.out.println("没有返回结果的任务 ");
         });
+        
+        //让线程池处理有返回结果的
+        Future<Object> future = threadPoolExecutor.submit(new Callable<Object>() {
+            @Override
+            public Object call() throws Exception {
+                return "我有返回结果";
+            }
+        });
+        Object result = future.get();
+        System.out.println(result);
+        //如果是局部的线程池，需要调用shutDown
+        threadPoolExecutor.shutdown();
+    }
+
+
+    private static class MyRejectExec implements RejectedExecutionHandler {
+
+        @Override
+        public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+
+        }
     }
 
 
