c522bde21157afa0a5bf039712ab2cdac7a8047e
wws <1096080443@qq.com>
2024-03-23 23:20:02 +0800

    git 切换

--- 数据结构与算法/src/main/java/com/carrry/slidong_windows/SlidingWindowMaximum.java
+++ 数据结构与算法/src/main/java/com/carrry/slidong_windows/SlidingWindowMaximum.java
@@ -42,7 +42,7 @@ public class SlidingWindowMaximum {
     public static void main(String[] args) {
         int [] nums = {1,3,-1,-3,5,3,6,7};
         int[] ints = maxSlidingWindow(nums, 3);
-        System.out.println();
+        System.out.println(1);
     }
 
 
