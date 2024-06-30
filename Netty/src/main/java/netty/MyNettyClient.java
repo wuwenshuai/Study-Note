aba98e112412e9822e421bc1fbd3f9fbdd7c8dfe
wws <1096080443@qq.com>
2024-06-30 13:09:05 +0800

    netty demo

--- Netty/src/main/java/netty/MyNettyClient.java
+++ Netty/src/main/java/netty/MyNettyClient.java
@@ -3,35 +3,59 @@ package netty;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.logging.LoggingHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.InetSocketAddress;
 
 public class MyNettyClient {
 
-
+    private static final Logger log = LoggerFactory.getLogger(MyNettyClient.class);
     public static void main(String[] args) throws InterruptedException {
 
         Bootstrap bootstrap = new Bootstrap();
 
         bootstrap.channel(NioSocketChannel.class);
-        bootstrap.group(new NioEventLoopGroup());
+        NioEventLoopGroup eventLoopGroup = new NioEventLoopGroup(3);
+        bootstrap.group(eventLoopGroup);
 
         bootstrap.handler(new ChannelInitializer<NioSocketChannel>() {
             @Override
             protected void initChannel(NioSocketChannel ch) throws Exception {
+                ch.pipeline().addLast(new LoggingHandler());
                 ch.pipeline().addLast(new StringEncoder());
             }
         });
 
+        //connect 用一个新的线程，用于连接
         ChannelFuture connect = bootstrap.connect(new InetSocketAddress(8000));
-        connect.sync();
-        Channel channel = connect.channel();
-        channel.writeAndFlush("hello carry");
+       // connect.sync(); //阻塞在这,链接建立就会走到后面代码,异步阻塞
 
+        //operationComplete 添加回调的方式等同上面操作，这一句话不是在主线程上面执行的，是 bootstrap.connect进行创建的
+        connect.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                log.debug("listen--------------");
+                //创建新线程进行写操作
+                Channel channel = connect.channel();
+                channel.writeAndFlush("hello carry");
+               // ChannelFuture close = channel.close();
+//                close.addListener(new ChannelFutureListener() {
+//                    @Override
+//                    public void operationComplete(ChannelFuture channelFuture) throws Exception {
+//                      log.debug("channel close-----");
+//                    }
+//                });
+            }
+        });
 
+        //优雅关闭
+     //   eventLoopGroup.shutdownGracefully();
     }
 }
--- Netty/src/main/java/netty/MyNettyServer.java
+++ Netty/src/main/java/netty/MyNettyServer.java
@@ -2,35 +2,92 @@ package netty;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.ChannelInitializer;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.util.NettyRuntime;
+import io.netty.util.internal.SystemPropertyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.nio.charset.Charset;
 
 public class MyNettyServer {
 
+    private static final Logger log = LoggerFactory.getLogger(MyNettyServer.class);
+
     public static void main(String[] args) {
+
         ServerBootstrap serverBootstrap = new ServerBootstrap();
         serverBootstrap.channel(NioServerSocketChannel.class);
 
-        // 创建了一组线程，通过死循环 监控accept read 事件
-        serverBootstrap.group(new NioEventLoopGroup());
 
+        NioEventLoopGroup bossEventGroup = new NioEventLoopGroup(1);
+
+        NioEventLoopGroup workerEventGroup = new NioEventLoopGroup(3);
+
+
+        // 获得default
+        DefaultEventLoopGroup  defaultEventLoopGroup = new DefaultEventLoopGroup(2);
+
+
+        // 创建了一组线程，通过死循环 监控accept read 事件
+        serverBootstrap.group(bossEventGroup,workerEventGroup);
+
+
+        //接收数据和写数据，都需要handler去处理
         serverBootstrap.childHandler(new ChannelInitializer<NioSocketChannel>() {
             @Override
+            //读写 Channel
             protected void initChannel(NioSocketChannel ch) throws Exception {
-                ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                ChannelPipeline pipeline = ch.pipeline();
+               pipeline.addLast(new StringDecoder());
+
+               //  ChannelInboundHandlerAdapter:读数据handler
+                pipeline.addLast("handler2", new ChannelInboundHandlerAdapter() {
                     @Override
                     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-
-                        String result = ((ByteBuf) msg).toString(Charset.defaultCharset());
-                        System.out.println("result = " + result);
+                        log.debug("handler2 ctx ... {} ", msg);
+                        super.channelRead(ctx, msg);
                     }
                 });
+
+                pipeline.addLast("handler3", new ChannelInboundHandlerAdapter() {
+                    @Override
+                    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                        log.debug("handler3");
+                        //super.channelRead(ctx, msg);
+                        ch.writeAndFlush("hello  suns"); //全局查找输出的OutboundHandler ，依次运行
+                        // ctx.writeAndFlush("hello xiaojr");  // c从当前的Handler，往前查找，没有输出的OutboundHandler
+                    }
+                });
+
+                pipeline.addLast("hadler4", new ChannelOutboundHandlerAdapter() {
+                    @Override
+                    public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                        log.debug("handler4");
+                        super.write(ctx, msg, promise);
+                    }
+                });
+                pipeline.addLast("hadler5", new ChannelOutboundHandlerAdapter() {
+                    @Override
+                    public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                        log.debug("handler5");
+                        super.write(ctx, msg, promise);
+                    }
+                });
+
+                pipeline.addLast("hadler6", new ChannelOutboundHandlerAdapter() {
+                    @Override
+                    public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                        log.debug("handler6");
+                        super.write(ctx, msg, promise);
+                    }
+                });
+
             }
         });
 
